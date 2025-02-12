5cbb65b092f482abdbb9d4be480d9adbf4bf1140
chonlaphoom <chonlaphoomth@gmail.com>
2025-02-12 20:05:29 +0700

    split all plugins

---
+++ nvim/lua/lazy-setup.lua
@@ -0,0 +1,31 @@
+-- install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	require("plugins.cmp"),
+	require("plugins.conform"),
+	require("plugins.gitsigns"),
+	require("plugins.lsp"),
+	require("plugins.mini"),
+	require("plugins.neo-tree"),
+	require("plugins.telescope"),
+	require("plugins.todo"),
+	require("plugins.treesitter"),
+	require("plugins.which-key"),
+	{
+		"cdmill/neomodern.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme coffeecat")
+		end,
+	},
+})
